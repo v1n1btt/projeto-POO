7d33ddfc2207a359c2d8dec9b18d742ac57e18ff
v1n1btt <vbbotte@gmail.com>
2024-10-11 10:04:26 -0300

    Add files via upload

---
+++ Turma.java
@@ -0,0 +1,36 @@
+public class Turma extends Curso{
+    private String sala;
+    private String horario;
+    private int quantidadeAlunos;
+
+    public Turma(String nome, String codigo, int cargaHoraria, String ementa, String preRequisitos, String sala, String horario, int quantidadeAlunos){
+        super(nome, codigo, cargaHoraria, ementa, preRequisitos);
+        setSala(sala);
+        setHorario(horario);
+        setQuantidadeAlunos(quantidadeAlunos);
+    }
+
+    public void setQuantidadeAlunos(int quantidadeAlunos) {
+        this.quantidadeAlunos = quantidadeAlunos;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
+
+    public int getQuantidadeAlunos() {
+        return quantidadeAlunos;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public String getSala() {
+        return sala;
+    }
+}
