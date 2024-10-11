d03f12dd8503d3ce821f85464564d19e48fb1749
LMStudios <160443408+LMStudios@users.noreply.github.com>
2024-10-11 09:23:54 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,56 @@
+public class Aluno {
+    private String nome;
+    private String codigo;
+    private int idade;
+    private String CPF;
+    private String plano;
+
+    public Aluno(String nome, String codigo, int idade, String CPF, String plano) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.idade = idade;
+        this.CPF = CPF;
+        this.plano = plano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getidade() {
+        return idade;
+    }
+
+    public void setidade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getplano() {
+        return plano;
+    }
+
+    public void setplano(String plano) {
+        this.plano = plano;
+    }
+
+}
---
+++ Curso.java
@@ -0,0 +1,56 @@
+public class Curso {
+    private String nome;
+    private String codigo;
+    private int cargaHoraria;
+    private String ementa;
+    private String preRequisitos;
+
+    public Curso(String nome, String codigo, int cargaHoraria, String ementa, String preRequisitos) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.cargaHoraria = cargaHoraria;
+        this.ementa = ementa;
+        this.preRequisitos = preRequisitos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getEmenta() {
+        return ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public String getPreRequisitos() {
+        return preRequisitos;
+    }
+
+    public void setPreRequisitos(String preRequisitos) {
+        this.preRequisitos = preRequisitos;
+    }
+
+}
