4bf115739986d1cc143f267e4b0709b8437e1a55
savioguilherme <sguilhermerodri015@gmail.com>
2024-10-30 16:19:57 -0300

    Mudança no contrutor de Professor

--- Projeto/Professor.java
+++ Projeto/Professor.java
@@ -6,7 +6,7 @@ public class Professor extends Pessoa {
 
     public Professor(String nome, String codigoUsuario, String email, String senhaPessoal,int cargaHoraria) {
         super(nome, codigoUsuario, email, senhaPessoal); 
-        this.cargaHoraria = cargaHoraria;
+        setCargaHoraria(cargaHoraria);
     }
 
     public void setCargaHoraria(int cargaHoraria) {
