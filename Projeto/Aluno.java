ff9d62437559e268a68d65514d41f26ea5f55328
savioguilherme <sguilhermerodri015@gmail.com>
2024-10-30 10:23:19 -0300

    Comentar a variável Idade em aluno até sua remoção ou volta!

--- Projeto/Aluno.java
+++ Projeto/Aluno.java
@@ -1,24 +1,24 @@
 package Projeto;
 public class Aluno extends Pessoa {
 
-    private int idade;
+    //private int idade;
     private String cpf;
     private String plano;
 
-    public Aluno(String nome, String codigoUsuario, String email, String senhaPessoal, int idade, String cpf, String plano) {
+    public Aluno(String nome, String codigoUsuario, String email, String senhaPessoal, /*int idade,*/ String cpf, String plano) {
         super(nome, codigoUsuario, email, senhaPessoal);
-        this.idade = idade;
+        //this.idade = idade;
         this.cpf = cpf;
         this.plano = plano;
     }
 
-    public int getIdade() {
+    /*public int getIdade() {
         return idade;
     }
 
     public void setIdade(int idade) {
         this.idade = idade;
-    }
+    }*/
 
     public String getCPF() {
         return cpf;
