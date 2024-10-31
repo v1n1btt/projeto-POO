1a708b26745c18ddb55ed362bd378d1b1b7e98f9
savioguilherme <sguilhermerodri015@gmail.com>
2024-10-31 11:02:08 -0300

    Mudanças nas classes Curso e Turma para se adequar a herença

--- Projeto/Curso.java
+++ Projeto/Curso.java
@@ -6,13 +6,16 @@ public class Curso {
     private String codigoCurso;
     private int cargaHorariaCurso;
     private String ementa;
-    private boolean status; 
+    private boolean status;
+    private int quantidadeAlunos;
 
-    public Curso(String nomeCurso, String codigoCurso, int cargaHorariaCurso, String ementa) {
+
+    public Curso(String nomeCurso, String codigoCurso, int cargaHorariaCurso, String ementa, int quantidadeAlunos) {
         setNomeCurso(nomeCurso);
         setCodigoCurso(codigoCurso);
         setCargaHorariaCurso(cargaHorariaCurso);
         setEmenta(ementa);
+        setQuantidadeAlunos(quantidadeAlunos);
         setStatus(true);
     }
 
@@ -48,12 +51,20 @@ public class Curso {
         this.ementa = ementa;
     }
 
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     public boolean getStatus() {
         return status;
     }
 
-    public void setStatus(boolean status) {
-        this.status = status;
+    public void setQuantidadeAlunos(int quantidadeAlunos) {
+        this.quantidadeAlunos = quantidadeAlunos;
+    }
+
+    public boolean getQuantidadeAlunos() {
+        return quantidadeAlunos;
     }
 
 }
--- Projeto/Turma.java
+++ Projeto/Turma.java
@@ -3,20 +3,10 @@ package Projeto;
 public class Turma extends Curso {
 
     private String horario;
-    private int quantidadeAlunos;
 
-    public Turma(String getNomeCurso, String codigoCurso, int cargaHorariaCurso, String ementa, String horario, int quantidadeAlunos) {
-        super(nomeCurso, codigoCursp, cargaHorariaCurso, ementa);
+    public Turma(String getNomeCurso, String codigoCurso, int cargaHorariaCurso, String ementa, boolean status, int quantidadeAlunos, String horario) {
+        super(nomeCurso, codigoCursp, cargaHorariaCurso, ementa, status, quantidadeAlunos);
         setHorario(horario);
-        setQuantidadeAlunos(quantidadeAlunos);
-    }
-
-    public void setQuantidadeAlunos(int quantidadeAlunos) {
-        this.quantidadeAlunos = quantidadeAlunos;
-    }
-
-    public int getQuantidadeAlunos() {
-        return quantidadeAlunos;
     }
 
     public void setHorario(String horario) {
